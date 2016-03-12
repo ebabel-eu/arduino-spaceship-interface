23747890145f360d567bc125b8abae580c1171fc
nadjibamar <hello@ebabel.eu>
2016-03-12 20:41:10 +0100

    code

--- spaceship-interface.ino
+++ spaceship-interface.ino
@@ -1,9 +1,44 @@
-void setup() {
-  // put your setup code here, to run once:
+int switchState = 0;
 
+void setup() {
+  pinMode(3, OUTPUT); // Green LED.
+  pinMode(4, OUTPUT); // First red LED.
+  pinMode(5, OUTPUT); // Second red LED.
+
+  pinMode(2, INPUT);  // Switch button is pushed or not pushed.
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  switchState = digitalRead(2);
 
+  if (switchState == LOW) { // The switch is not pushed.
+
+    digitalWrite(3, HIGH);  // Green LED is on.
+    digitalWrite(4, LOW);   // First red LED is off.
+    digitalWrite(5, LOW);   // Second red LED is off.
+
+  } else {  // The switch is pushed.
+
+    // Green LED is always off while switch is pushed.
+    digitalWrite(3, LOW);
+
+    // One red LED is off, the other is on.
+    digitalWrite(4, LOW);
+    digitalWrite(5, HIGH);
+
+    delay(250); // Pause for a quarter of a second.
+
+    // One red LED is on, the other is off.
+    digitalWrite(4, HIGH);
+    digitalWrite(5, LOW);
+
+    delay(250); // Pause for a quarter of a second.
+
+  }
 }
+
+
+
+
+
+
